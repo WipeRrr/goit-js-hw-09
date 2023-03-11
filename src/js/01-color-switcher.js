16681ec93f0521db5ad5e4eff2e4f5a420420eae
Anatoliy Matsakov <mactolik1993@gmail.com>
2023-03-11 17:16:46 +0200

    1 task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,58 +1,29 @@
-console.log('gde console???');
-console.log('asdqwe');
+const bodySelector = document.querySelector('body');
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
 
+let timerId = null;
 
+btnStart.addEventListener('click', startColor);
 
+btnStop.addEventListener('click', stopColor);
 
+function stopColor() {
+  btnStop.disabled = true;
+  btnStart.disabled = false;
+  clearInterval(timerId);
+}
 
+function startColor() {
+  timerId = setInterval(ChangeColor, 1000);
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+}
 
+function ChangeColor() {
+  bodySelector.style.backgroundColor = getRandomHexColor();
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const refs = {
-//   bodySelector: document.querySelector('body'),
-//   btnStart: document.querySelector('data-start'),
-//   btnStop: document.querySelector('data-stop'),
-// };
-
-// let timerId = null;
-// console.log(btnStart);
-// refs.btnStart.addEventListener('click', () => {
-//   console.log('asdasd')
-//   timerId = setTimeout(ChangeColor, 1000);
-//   setTimeout();
-// });
-
-// function ChangeColor() {
-//   bodySelector.style.backgroundColor = getRandomHexColor();
-// }
-
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
